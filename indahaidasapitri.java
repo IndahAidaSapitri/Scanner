d3e331f528956900d64caf74fdaa9f8d062fe5f8
IndahAidaSapitri <56386662+IndahAidaSapitri@users.noreply.github.com>
2019-10-11 19:00:02 +0700

    Tugas Daspro
    Nama : Indah Aida Sapitri
    Nim : 11950121695

---
+++ indahaidasapitri.java
@@ -0,0 +1,25 @@
+
+package javaapplication5;
+
+import java.util.Scanner;
+
+
+
+public class JavaApplication5 {
+
+    public static void main(String[] args) {
+    Scanner merkhp = new Scanner(System.in);
+    Scanner merkhp1 = new Scanner(System.in);
+    Scanner merkhp2 = new Scanner(System.in);
+    System.out.println("masukkan merkhp : ");
+    System.out.println("masukkan merkhp1 : ");
+    System.out.println("masukkan merkhp2 : ");
+    String nama = merkhp.nextLine();
+    String nama1 = merkhp1.nextLine();
+    String nama2 = merkhp2.nextLine();
+    System.out.println("merk hp nya adalah " + nama);
+    System.out.println("merk hp1 nya adalah " + nama1);
+    System.out.println("merk hp2 nya adalah " + nama2);
+    }
+    
+}
